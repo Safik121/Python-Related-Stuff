7e81897294e4f565eaff77c0419d245ad6eadcd7
Safik121 <105844179+Safik121@users.noreply.github.com>
2023-09-15 08:11:13 +0200

    Create box.py

---
+++ Python-Files-School/box.py
@@ -0,0 +1,10 @@
+from turtle import Turtle
+
+BolonskySpagety = Turtle()
+
+BolonskySpagety.width(4)
+BolonskySpagety.color('pink')
+
+for abcdefg in range(4):
+    BolonskySpagety.forward(100)
+    BolonskySpagety.left(90)
