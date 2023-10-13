7e0e02dc32275702c4316f873072e13e6d8e50c2
Safik121 <105844179+Safik121@users.noreply.github.com>
2023-10-13 09:56:01 +0200

    Update nasobilka.py

--- Python-Files-School/nasobilka.py
+++ Python-Files-School/nasobilka.py
@@ -2,19 +2,26 @@ def table_products(n):
     prvni_var = 1
     druha_var = 0
 
-    print("     ", end="")
+    print("    ", end="")
     for i in range(n):
         print(prvni_var+i, end=" ")
     print("")
-    print("     ", end="")
+    print("    ", end="")
     for o in range(n):
         print("-", end=" ")
     print("")
-    for p in range(1+druha_var, (n+1)*prvni_var, prvni_var):
+    """
+    for p in range(1+druha_var, (n+1)*prvni_var):
         print(p, " / ", end="")
         for s in range(1+druha_var, (n+1)*prvni_var, prvni_var):
             print(s, end=" ")
         print("")
         druha_var+=1
         prvni_var+=1
+    """
+    for i in range(1,n+1):
+        print(i,"/ ",end="")
+        for x in range(1,n+1):
+            print(i*x,end=" ")
+        print(" ")
 table_products(5)
