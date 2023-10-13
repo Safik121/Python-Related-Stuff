99e507acd88a20bff7b3d804b1ba825bd04f2321
Safik121 <105844179+Safik121@users.noreply.github.com>
2023-10-13 10:08:53 +0200

    Create scitani.py

---
+++ Python-Files-School/scitani.py
@@ -0,0 +1,16 @@
+def table_products(n):
+    prvni_var = 1
+    print("    ", end="")
+    for i in range(n):
+        print(prvni_var+i, end=" ")
+    print("")
+    print("    ", end="")
+    for i in range(n):
+        print("-", end=" ")
+    print("")
+    for i in range(1,n+1):
+        print(i,"/ ",end="")
+        for x in range(1,n+1):
+            print(i+x,end=" ")
+        print(" ")
+table_products(5)
