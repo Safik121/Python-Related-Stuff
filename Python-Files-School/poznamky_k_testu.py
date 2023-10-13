c12911bcdaf884f038b2904c701ba0c8c313a61e
Safik121 <105844179+Safik121@users.noreply.github.com>
2023-10-13 08:57:22 +0200

    Update poznamky_k_testu.py

--- Python-Files-School/poznamky_k_testu.py
+++ Python-Files-School/poznamky_k_testu.py
@@ -7,5 +7,5 @@ print(3//4) # // "Celočíselné dělení" = (Vždy napíše celé číslo, nena
 print(4%2) # % "Modulo" = Zbytek po dělení (4/2 = nemá zbytek) - 9/3 = 3 a zbytek 1
 print(5&3) #bitová operace AND (musí být 2 jedničky, aby byla vypsaná 1)
 print(5|3) #bitova operace OR (stačí 1 jednička, aby byla vypsaná 1)
-print(5>>2) #shiftování doprava, posune bit doprava 00000101 (5) na 00000010 (2)
-print(5<<3) #shiftování doleva, posune bit doleva 00000101 (5) na 00001010 (10)
+print(5>>1) #shiftování doprava, posune bit doprava 00000101 (5) na 00000010 (2)
+print(5<<1) #shiftování doleva, posune bit doleva 00000101 (5) na 00001010 (10)
