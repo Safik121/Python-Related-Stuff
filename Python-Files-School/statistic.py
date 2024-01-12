f60022b9b62b8ca3617fc31a1481d70773f1dfef
Safik121 <105844179+Safik121@users.noreply.github.com>
2024-01-12 21:10:44 +0100

    Create statistic.py

---
+++ Python-Files-School/statistic.py
@@ -0,0 +1,14 @@
+from random import randint
+
+
+def statistic(count, lower, upper):
+    a = []
+    s = 0
+    for i in range(1, count + 1):
+        x = randint(lower, upper)
+        a.append(x)
+        s += x
+        print(x, end=" ")
+    print("\n", min(a), end=" - Nejmenší číslo, ")
+    print(max(a), end=" - Největší číslo, ")
+    print(s/count, end=" - Průměr" )
