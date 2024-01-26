2b24f9cb173ac7e789af68f4b93fbbfa474cb772
Safik121 <105844179+Safik121@users.noreply.github.com>
2024-01-26 10:27:03 +0100

    Update rubbish_injector.py

--- Python-Files-School/rubbish_injector.py
+++ Python-Files-School/rubbish_injector.py
@@ -1,7 +1,8 @@
 def rubbish_injector(text, rubbish):
     result = ""
-    for i in text:
-        result += i+rubbish
+    for i in range(len(text)-1):
+        result += text[i] + rubbish
+    result += text[-1]
     print(result)
 
 rubbish_injector("AHOJ", "X")
