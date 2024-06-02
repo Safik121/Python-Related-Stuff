2c4063591e2bbd8d488a60640ec3b00a21f35f9e
Safik121 <105844179+Safik121@users.noreply.github.com>
2024-06-02 13:14:37 +0200

    Create binary_needle_idk.py

---
+++ Python-Files-School/binary_needle_idk.py
@@ -0,0 +1,21 @@
+def binary_search(needle, haystack):
+    left, right = 0, len(haystack) - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+
+        if haystack[mid] == needle:
+            return True
+        elif haystack[mid] < needle:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return False
+
+haystack = [1, 2, 5, 8]
+needle = 5
+print(binary_search(needle, haystack))
+
+needle = 3
+print(binary_search(needle, haystack))
